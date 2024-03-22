2e2c6c3f818191998f395ee8812cafb114944394
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-03-22 16:05:02 -0700

    Simplify a bit

--- 8pin-pio/main.py
+++ 8pin-pio/main.py
@@ -5,6 +5,12 @@ from time import sleep
 p = []
 MAX_PINS = 8  # offset by one because pins start at zero
 
+def blink(pin, sleepytime=0.1):
+    """Blink pin"""
+    pin.on()
+    sleep(sleepytime)
+    pin.off()
+    
 
 def main():
     """
@@ -13,21 +19,11 @@ def main():
     p = [Pin(i, Pin.OUT) for i in range(0, MAX_PINS)]
 
     while True:
-        for i in range(0, MAX_PINS):
-            print(i)
-            p[i].on()
-            sleep(0.1)
-            p[i].off()
+        for i in p:
+            blink(i)
 
         sleep(0.2)
-
-        for i in range(MAX_PINS-1, -1, -1):
-            print(i)
-            p[i].on()
-            sleep(0.1)
-            p[i].off()
-
-        sleep(0.2)
+        p.reverse()
 
 
 if __name__ == "__main__":
