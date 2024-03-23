4ca7a01edb62dc2ccbcca0834f825148e05ee942
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-03-22 17:08:33 -0700

    Working blink PIO

--- 8pin-pio/main.py
+++ 8pin-pio/main.py
@@ -1,8 +1,33 @@
 from machine import Pin
+from rp2 import PIO, StateMachine, asm_pio
 from time import sleep
 
-
 MAX_PINS = 8  # offset by one because pins start at zero
+
+@asm_pio(set_init=PIO.OUT_LOW)
+def pioblink():
+    wrap_target()
+    set(pins, 1) [31]
+    nop() [31]
+    nop() [31]
+    nop() [31]
+    nop() [31]
+    set(pins, 0) [31]
+    nop() [31]
+    nop() [31]
+    nop() [31]
+    nop() [31]
+    wrap()
+
+@asm_pio(
+    out_init=(PIO.OUT_LOW) * 8,
+    out_shiftdir=PIO.SHIFT_RIGHT,
+    autopull=True,
+    pull_thresh=16,
+)
+def paral_prog():
+    pull()
+    out(pins, 8)
 
 
 def blink(outp, sleepytime=0.1):
@@ -22,22 +47,38 @@ def blink_and_check(outp, inp, sleepytime=0.1):
 
 
 def main():
-    """
-    Main entry point
-    """
-    outputs = [Pin(i, Pin.OUT) for i in range(0, MAX_PINS)]
-    inputs = [Pin(i + 8, Pin.IN) for i in range(0, MAX_PINS)]
+    # paral_sm = StateMachine(0, paral_prog, freq=2000, out_base=Pin(0))
+    # paral_sm.active(1)
 
-    while True:
-        vals = []
-        for o, i in zip(outputs, inputs):
-            vals.append(blink_and_check(o, i))
+    sm = StateMachine(0, pioblink, freq=2000, set_base=Pin(0))
+    sm.active(1)
+    sleep(5)
+    sm.active(0)
 
-        print(vals)
+    # while True:
+    #     for i in range(500):
+    #         paral_sm.put(i)
+    #         print(i)
+    #         sleep(0.5)
 
-        sleep(0.2)
-        outputs.reverse()
-        inputs.reverse()
+
+# def main():
+#     """
+#     Main entry point
+#     """
+#     outputs = [Pin(i, Pin.OUT) for i in range(0, MAX_PINS)]
+#     inputs = [Pin(i + 8, Pin.IN) for i in range(0, MAX_PINS)]
+
+#     while True:
+#         vals = []
+#         for o, i in zip(outputs, inputs):
+#             vals.append(blink_and_check(o, i))
+
+#         print(vals)
+
+#         sleep(0.2)
+#         outputs.reverse()
+#         inputs.reverse()
 
 
 if __name__ == "__main__":
