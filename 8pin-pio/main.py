592238fe1ef9b7e1e40de0846c14b0bed6a42f0e
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-03-22 17:21:28 -0700

    Add comment to emphasize out_init requiring a comma
    No idea what's going on here.
    https://docs.micropython.org/en/v1.17/library/rp2.html is not much help.

--- 8pin-pio/main.py
+++ 8pin-pio/main.py
@@ -19,7 +19,13 @@ def pioblink():
     nop() [31]
     wrap()
 
+
 @asm_pio(
+    # https://docs.micropython.org/en/v1.17/library/rp2.html: if more
+    # than one pin is used in the program, out_init() needs to be a
+    # tuple.  That explains the requirement of a comma for this to
+    # work; I presume that the single element in the tuple means "they
+    # should all be like this".
     out_init=(PIO.OUT_HIGH,) * 8,
     out_shiftdir=PIO.SHIFT_RIGHT,
     autopull=True,
