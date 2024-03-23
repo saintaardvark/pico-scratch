12c83a733d1f5fb35c1406f0f05d7f98b1f1a735
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-03-23 16:07:11 -0700

    Add another SM to read parallel pins

--- 8pin-pio/main.py
+++ 8pin-pio/main.py
@@ -4,6 +4,7 @@ from time import sleep
 
 MAX_PINS = 8  # offset by one because pins start at zero
 
+
 @asm_pio(
     # https://docs.micropython.org/en/v1.17/library/rp2.html: if more
     # than one pin is used in the program, out_init() needs to be a
@@ -20,14 +21,29 @@ def paral_prog():
     out(pins, 8)
 
 
+@asm_pio(
+    set_init=PIO.OUT_HIGH, in_shiftdir=PIO.SHIFT_RIGHT, autopush=True, push_thresh=16
+)
+def paral_read():
+    """
+    Read in parallel
+    """
+    in_(pins, 8)
+    push()
+
+
 def main():
     paral_sm = StateMachine(0, paral_prog, freq=2000, out_base=Pin(0))
+    paral_read_sm = StateMachine(1, paral_read, freq=2000, in_base=Pin(8))
     paral_sm.active(1)
+    paral_read_sm.active(1)
 
     while True:
         for i in range(500):
             paral_sm.put(i)
-            print(i)
+            print(f"i = {i}")
+            r = paral_read_sm.get()
+            print(f"Read: {r}")
             sleep(0.5)
 
 
