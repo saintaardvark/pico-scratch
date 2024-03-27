3101ad34a917ed91c28f7e3ac4fc6629e70848b9
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-03-26 20:50:01 -0700

    Initial version of 8 pin PIO approch
    Not yet working, but I think I've got the right idea here.

--- 8pin-pio/main.py
+++ 8pin-pio/main.py
@@ -2,32 +2,33 @@ from machine import Pin
 from rp2 import PIO, StateMachine, asm_pio
 from time import sleep
 
-from paral import paral_prog, paral_read
+from paral import clock, paral_read
 
 MAX_PINS = 8  # offset by one because pins start at zero
+JMP_PIN = Pin(8)
 
 
 def main():
-    paral_sm = StateMachine(0, paral_prog, freq=2000, out_base=Pin(0))
-    paral_read_sm = StateMachine(1, paral_read, freq=2000, in_base=Pin(8))
-    paral_sm.active(1)
-    paral_read_sm.active(1)
+    read_sm = StateMachine(
+        0, paral_read, freq=2000, in_base=Pin(0), sideset_base=JMP_PIN
+    )
+    clock_sm = StateMachine(1, clock, freq=2000, jmp_pin=JMP_PIN)
+    # TODO: set both active at once
+    read_sm.active(1)
+    clock_sm.active(1)
     last = -1
     while True:
-        for i in range(256):
-            paral_sm.put(i)
-            print(f"i = {i}")
-            r = paral_read_sm.get()
-            r = r << 24
-            if last == -1:
-                last = r
-            # print(f"Read: {r}")
-            # We're reading in 8 bits; the TX buffer is 32 bits;
-            # shift by 24.
-            # print(f"Shift: {r >> 24}")
-            if last != r:
-                print(r)
-            sleep(0.5)
+        r = paral_read_sm.get()
+        r = r << 24
+        if last == -1:
+            last = r
+        # print(f"Read: {r}")
+        # We're reading in 8 bits; the TX buffer is 32 bits;
+        # shift by 24.
+        # print(f"Shift: {r >> 24}")
+        if last != r:
+            print(r)
+        sleep(0.5)
 
 
 if __name__ == "__main__":
--- 8pin-pio/paral.py
+++ 8pin-pio/paral.py
@@ -12,17 +12,31 @@ from rp2 import PIO, StateMachine, asm_pio
     autopull=True,
     pull_thresh=16,
 )
-def paral_prog():
-    pull()
-    out(pins, 8)
-
+def clock():
+    set(x, 0)
+    label("WAIT_FOR_P1_HIGH")
+    # Wait for pin1 to be high
+    jmp(pin, "PUSH")
+    jmp(x_dec, "WAIT_FOR_P1_HIGH")
+    label("PUSH")
+    in(x, 32)
+    push()
+    jmp(x_dec, "WAIT_FOR_P1_HIGH")
 
 @asm_pio(
-    set_init=PIO.OUT_HIGH, in_shiftdir=PIO.SHIFT_RIGHT, autopush=True, push_thresh=16
+    set_init=PIO.OUT_LOW, in_shiftdir=PIO.SHIFT_RIGHT, autopush=True, push_thresh=16
 )
 def paral_read():
     """
     Read in parallel
     """
-    in_(pins, 8)
+    mov(y, pins)
+    label("main_loop")
+    mov(x, pins)
+    jmp(x_not_y, "move_out").side(1)
+    jmp("main_loop")
+    label("move_out")
+    in_(x, 8).side(0)
     push()
+    mov(y, x)
+    jmp("main_loop")
